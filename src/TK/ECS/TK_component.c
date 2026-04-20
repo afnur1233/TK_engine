47d2a444b65d16278d7a2e40d1fcdfd1bebc69a8
Afnur <afnur1233@gmail.com>
2026-04-20 20:14:02 +0300

    Begin implementing entity component system

--- src/TK/ECS/TK_component.c
+++ src/TK/ECS/TK_component.c
@@ -37,3 +37,30 @@ void TK_Entity_SetCubeMesh( TK_EntityContainer *container, TK_Entity entity )
     
     data->bitset |= TK_EntityComponentBit_CubeMesh;
 }
+
+
+void TK_Entity_GetPosition3( TK_EntityContainer *container, TK_Entity entity, vec3 dest )
+{
+    TK_DebugAssert( container != NULL );
+    TK_DebugAssert( ( entity / TK_ENTITY_PAGE_SIZE ) < container->pages_cap, "Invalid entity ID" );
+    
+    TK_EntityData *page = container->pages[entity / TK_ENTITY_PAGE_SIZE];
+    TK_EntityData *data = &page[entity % TK_ENTITY_PAGE_SIZE];
+    
+    data->bitset |= TK_EntityComponentBit_Position3;
+    
+    glm_vec3_dup(data->position3, position3);
+}
+
+void TK_Entity_GetScaling3( TK_EntityContainer *container, TK_Entity entity, vec3 dest )
+{
+    TK_DebugAssert( container != NULL );
+    TK_DebugAssert( ( entity / TK_ENTITY_PAGE_SIZE ) < container->pages_cap, "Invalid entity ID" );
+    
+    TK_EntityData *page = container->pages[entity / TK_ENTITY_PAGE_SIZE];
+    TK_EntityData *data = &page[entity % TK_ENTITY_PAGE_SIZE];
+    
+    data->bitset |= TK_EntityComponentBit_Scaling3;
+    
+    glm_vec3_dup(data->scaling3, scaling3);
+}
