154fa057176578917fc0fdcd3e1e9ee5cc0e887d
Afnur <afnur1233@gmail.com>
2026-04-20 14:28:31 +0300

    Add license

--- src/Sandbox/Main.c
+++ src/Sandbox/Main.c
@@ -1,7 +1,21 @@
 #include <TK/TK.h>
 
+typedef struct TK_EntityContainer TK_EntityContainer;
+
+struct TK_EntityContainer
+{
+    
+};
+
+#define TK_EntityContainer(...)
+
 int main()
 {
     TK_AllocatorTracker tracker = TK_AllocatorTracker( &TK_AllocatorLibc );
     TK_Allocator *allocator = &tracker.allocator;
+    
+    TK_EntityContainer ECS = TK_EntityContainer( .allocator = allocator );
+    
+    TK_IFile file = TK_IOpen("Shader.glsl")
+    TK_IStream *stream = &file.stream;
 }
